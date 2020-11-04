1f5d8ec7d4bb77ea97d85356820bdc4dfd3189f5
“Emmanuel <emmarthurson@gmail.com>
2020-11-04 08:12:13 +0000

    hotfix

--- database/seeds/ProductCategoryTableSeeder.php
+++ database/seeds/ProductCategoryTableSeeder.php
@@ -173,10 +173,13 @@ class ProductCategoryTableSeeder extends Seeder
 
         foreach ($categories as $cat) {
             $category = ProductCategory::create($cat);
-           if(Storage::exists('categories/'. $cat['name'] . '.jpg')){
-                $category = ProductCategory::create($cat);
-                $category->addMedia(storage_path('app/categories/'. $cat['name'] . '.jpg'), 'local')
-                ->toMediaCollection('featured');
+           if(Storage::exists('product_categories/'. $cat['name'] . '.jpg')){
+               if(!(ProductCategory::where('name', $cat['name'])->first())){
+                    $category = ProductCategory::create($cat);
+                    $category->addMedia(storage_path('app/product_categories/'. $cat['name'] . '.jpg'), 'local')
+                    ->toMediaCollection('featured');
+               };
+
            }
         }
     }
