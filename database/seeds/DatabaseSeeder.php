c77d8d2efec0d25fa712a48e751ad43d43403551
“Emmanuel <emmarthurson@gmail.com>
2020-11-04 08:07:20 +0000

    hotfix

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,8 +12,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(CountriesTableSeeder::class);
-        $this->call(ProductCategoryTableSeeder::class);
-        $this->call(ProductTableSeeder::class);
         $this->call(UserTableSeeder::class);
     }
 }
