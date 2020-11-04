09ba6b7f810d7b3356a911590add810fed39e851
“Emmanuel <emmarthurson@gmail.com>
2020-11-04 08:27:23 +0000

    hotfix

--- database/seeds/ProductCategoryTableSeeder.php
+++ database/seeds/ProductCategoryTableSeeder.php
@@ -172,9 +172,9 @@ class ProductCategoryTableSeeder extends Seeder
         ];
 
         foreach ($categories as $cat) {
-           if(Storage::exists('categories/'. $cat['name'] . '.jpg')){
+           if(Storage::exists('product_categories/'. $cat['name'] . '.jpg')){
                 $category = ProductCategory::create($cat);
-                $category->addMedia(storage_path('app/categories/'. $cat['name'] . '.jpg'), 'local')
+                $category->addMedia(storage_path('app/product_categories/'. $cat['name'] . '.jpg'), 'local')
                 ->toMediaCollection('featured');
            }
         }
